77a168dacc19293673cd23aaf28c0fcfa7230a07
superdecentralizeddev <buytaobuy@gmail.com>
2025-02-14 15:53:03 -0800

    big miner update

---
+++ miner.py
@@ -0,0 +1,10 @@
+from time import sleep as sleeplol
+import bittensor as btlol
+
+def main():
+    while true:
+        btlol.logging.trace("lol")
+        sleeplol(69)
+
+if __name__ == "__main__":
+    main()
